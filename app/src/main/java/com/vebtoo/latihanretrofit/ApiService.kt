bbe8d87693c6f05bb0ee22bc0957ecc467cb01bf
herdo dimas pratirto <herdodimas46@gmail.com>
2024-11-11 15:21:57 +0700

    baru diperbarui

--- app/src/main/java/com/vebtoo/latihanretrofit/ApiService.kt
+++ app/src/main/java/com/vebtoo/latihanretrofit/ApiService.kt
@@ -2,8 +2,9 @@ package com.vebtoo.latihanretrofit
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService{
     @GET("artikel.php") //sesuai dengan endpoint yang digunakan
-    fun getArtikel(): Call<ArtikelResponse>
+    fun getArtikel(@Query("i") i: Int = 1): Call<ArtikelResponse>
 }
